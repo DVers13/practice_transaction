b2535a273334bb35134957af3e354792f2fe2e20
DVers13 <91010704+DVers13@users.noreply.github.com>
2024-07-05 14:02:24 +0600

    Набросок внешнего вида приложения

--- app/app.py
+++ app/app.py
@@ -5,13 +5,21 @@ import time
 import pandas as pd
 
 def main():
-    st.title('Transaction')
+    st.title('Transaction', )
     file = st.file_uploader("Upload csv file", type="csv")
 
     if st.button('Write data to PostgreSQL'):
-        url = "http://127.0.0.1:8000/transaction/upload_csv/"
-        files = {'file': file.getvalue()}
-        response = requests.post(url, files=files)
-        st.text(response)
+        with st.spinner("Just a moment ..."): 
+            url = "http://127.0.0.1:8000/transaction/upload_csv/"
+            files = {'file': file.getvalue()}
+            response = requests.post(url, files=files)
+        st.success('Done!')
+    if st.button('Find Fraud'):
+        with st.spinner("Just a moment ..."): 
+            time.sleep(1)
+        st.success('Done!')
+        st.text("Сдесь выведем подозрительные активности и их процент(или цвет)")
+        st.text("Можно визуализировать какие нибудь графики")
+        
 if __name__ == "__main__":
     main()
